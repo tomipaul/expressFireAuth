c6be915f74d18de34d0e7648ba3aac957865f9db
tomi <tomipaul95@gmail.com>
2017-05-29 23:51:48 +0100

    [feat]: Add middewares that handle user sign-up
    Implement a sign up function that creates and returns an array of middlewares to handle user sign-up.
    The returned middlewares validate request, create user and send a token to client as cookie or json body

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import NodeRSA from 'node-rsa';
 
 const expressFireAuth = (firebaseApp) => {
   // Get PEM encoded private key for RSA
-  const key = new NodeRSA({ b: 1026 }, 'pkcs1-private-pem');
+  const key = new NodeRSA({ b: 1024 }, 'pkcs1-private-pem');
   const exported = key.exportKey('pkcs1-private-pem');
 
   // Generate token
@@ -17,6 +17,53 @@ const expressFireAuth = (firebaseApp) => {
     };
     const token = jwt.sign(privateClaim, exported, options);
     return token;
+  };
+
+  // Create a new user in your express-firebase app
+  const signUp = ({
+    setCookies = false,
+    redirect = false,
+    path = '/home'
+  } = {}, ...middlewares) => {
+    // Check request method and payload
+    const validateRequest = (req, res, next) => {
+      if (req.method !== 'POST') {
+        throw new Error('POST request method expected');
+      } else if (!req.body.email || !req.body.password) {
+        throw new Error('non-empty email and password expected');
+      } else {
+        next();
+      }
+    };
+    // Create user
+    const createUser = (req, res, next) => {
+      const { email, password } = req.body;
+      const auth = firebaseApp.auth();
+      auth.createUserWithEmailAndPassword(email, password)
+      .then((user) => {
+        const token = generateToken(user);
+        req.token = token;
+        next();
+      })
+      .catch((err) => {
+        next(err);
+      });
+    };
+    // Wrap up request after user sign up
+    const response = (req, res) => {
+      if (setCookies) {
+        const date = new Date();
+        date.setDate(date.getDate() + 30);
+        res.cookie('token', req.token, { expires: date, httpOnly: true });
+        return (redirect) ? res.redirect(path) : res.sendStatus(200);
+      }
+      return res.json(req.token);
+    };
+    return [validateRequest, createUser, ...middlewares, response];
+  };
+  // Return api
+  return {
+    signUp
   };
 };
 
