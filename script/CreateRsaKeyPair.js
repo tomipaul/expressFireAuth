c97b7218edaa7df83d391c201339bcc82edf1496
tomi <tomipaul95@gmail.com>
2017-06-05 22:35:13 +0100

    [feat]: Create a file with rsa key pair
    file is not created
    * If such file exists already
    * If rsa keys have been set as environment variables- PUBLIC_KEY and PRIVATE_KEY.

---
+++ script/CreateRsaKeyPair.js
@@ -0,0 +1,14 @@
+import fs from 'fs';
+import { generateRSAKeyPair } from '../helpers';
+
+const keyDirectory = `${__dirname}/../key`;
+
+if (!process.env.PRIVATE_KEY &&
+ !fs.existsSync(`${keyDirectory}/rsapair.pem`)) {
+  const rsaKeyPair = generateRSAKeyPair();
+  const publicKey = JSON.stringify(rsaKeyPair.publicKey);
+  const privateKey = JSON.stringify(rsaKeyPair.privateKey);
+  const data = `PRIVATE_KEY = ${privateKey}\nPUBLIC_KEY = ${publicKey}`;
+  fs.mkdirSync(keyDirectory);
+  fs.writeFileSync(`${keyDirectory}/rsapair.pem`, data);
+}
